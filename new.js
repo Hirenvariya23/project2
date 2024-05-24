425c38c91aeaa8ccc9b71be0e572d371324f0da2
HirenVariya24 <167958851+Hirenvariya23@users.noreply.github.com>
2024-05-24 18:27:25 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,78 @@
+/*let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+windows.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+               links.classList.remove('active');
+               document.querySelector('header nav ul li a[href*=' + id + ']').classList.add(active)
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}*/
+
+ //for loop//
+
+/*for( i = 20; i<76; i++){
+    console.log(i);
+}*/
+
+//while loop//
+
+//var a = 25;//
+
+/*while(a<20){
+   a++;
+}*/  
+
+
+
+/*function iskaganabajao(){
+  console.log('ganabajao');
+}
+iskaganabajao();*/
+
+
+
+/*function yerozkaro(){
+    console.log('subhe jaldi utho');
+    console.log('fir running karne jao');
+    console.log('fir ghar aao or fresh ho jao');
+    console.log('fir breakfast karo');
+    console.log('fir study karo');
+}
+yerozkaro(); 
+yerozkaro(); 
+yerozkaro(); */
+
+
+
+/*function abcd(a,b,c,d,e){
+  console.log(a,b,c,d,e); 
+}
+abcd(15, 18, 178, 168, 695);*/
+
+var array = [1,2,3,4,5,6,19];
+ 
+array.splice(2,5);
+
+
+
+
+
+
+
